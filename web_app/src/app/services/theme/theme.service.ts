e08e5bd96de56ee59a769ae8a5f1c5a95891b96e
hilkojj <hilkojj@outlook.com>
2018-06-15 20:09:47 +0200

    huh

--- web_app/src/app/services/theme/theme.service.ts
+++ web_app/src/app/services/theme/theme.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as $ from 'jquery';
 import { HttpClient } from '@angular/common/http';
-import { resolve } from 'dns';
-import { reject } from 'q';
 
 const styleSheetURL = "/assets/css/dynamic-stylesheet.css";
 
