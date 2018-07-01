09ff631eee50eb14d610a532b4a3100e5dbf9bca
hilkojj <hilkojj@outlook.com>
2018-07-01 19:39:50 +0200

    waste of time

--- web_app/src/app/components/navbar/navbar.component.ts
+++ web_app/src/app/components/navbar/navbar.component.ts
@@ -1,26 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilsService } from '../../services/utils.service';
 import { Tab } from '../tabs/tabs.component';
+import { tWords, pWords } from './random-words';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(
-      private utils: UtilsService
-  ) { }
+    constructor(
+        private utils: UtilsService
+    ) { }
 
-  tabs: Tab[] = [
-      new Tab("Hoom", "home", "/hoom"),
-      new Tab("Amusement", "subscriptions", "/amusement"),
-      new Tab("Tjets", "chat_bubble", "/tjets"),
-      new Tab("Mesnen & vriends", "group", "/vriends")
-  ];
+    randomWords = "";
+    randomWordsTimeout;
 
-  ngOnInit() {
-  }
+    tabs: Tab[] = [
+        new Tab("Hoom", "home", "/hoom"),
+        new Tab("Amusement", "subscriptions", "/amusement"),
+        new Tab("Tjets", "chat_bubble", "/tjets"),
+        new Tab("Mesnen & vriends", "group", "/vriends")
+    ];
+
+    ngOnInit() {
+    }
+
+    showRandomWords() {
+        this.randomWords = tWords[Math.floor(Math.random() * tWords.length)] + " " + pWords[Math.floor(Math.random() * pWords.length)];
+        clearTimeout(this.randomWordsTimeout);
+        this.randomWordsTimeout = setTimeout(() => {
+            this.randomWords = "";
+        }, 2000);
+    }
 
 }
