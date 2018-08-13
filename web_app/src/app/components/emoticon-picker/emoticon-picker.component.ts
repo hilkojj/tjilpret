b4581c5dd763db04c0d40c45c97600b98bc91d64
hilkojj <hilkojj@outlook.com>
2018-08-13 19:49:08 +0200

    emoticon picker done

--- web_app/src/app/components/emoticon-picker/emoticon-picker.component.ts
+++ web_app/src/app/components/emoticon-picker/emoticon-picker.component.ts
@@ -1,23 +1,43 @@
-import { Component, OnInit, Input, HostListener, ElementRef } from '@angular/core';
+import { Component, Input, HostListener, ElementRef, Output, EventEmitter } from '@angular/core';
+import { EmoticonsService } from '../../services/emoticons.service';
+import { EmoticonCategory } from '../../models/emoticons';
+import { UtilsService } from '../../services/utils.service';
+import { DomEventsPlugin } from '../../../../node_modules/@angular/platform-browser/src/dom/events/dom_events';
 
 @Component({
     selector: 'app-emoticon-picker',
     templateUrl: './emoticon-picker.component.html',
     styleUrls: ['./emoticon-picker.component.scss']
 })
-export class EmoticonPickerComponent implements OnInit {
+export class EmoticonPickerComponent {
 
     @Input() input: HTMLTextAreaElement;
+    @Output() inputChange = new EventEmitter<string>();
 
     showPicker = false;
+    emoticons: EmoticonCategory[];
+    searchQuery = "";
+    activeCatId = 0;
 
     private thisElement: Element;
 
-    constructor(elRef: ElementRef) {
+    constructor(
+        elRef: ElementRef,
+        public service: EmoticonsService,
+
+        private utils: UtilsService
+
+    ) {
         this.thisElement = elRef.nativeElement;
     }
 
-    ngOnInit() {
+    toggle() {
+        this.showPicker = !this.showPicker;
+
+        if (!this.showPicker) return;
+
+        this.emoticons = this.service.emoticons;
+        this.activeCatId = 0 in this.emoticons ? this.emoticons[0].id : 0;
     }
 
     insert(name: string) {
@@ -25,10 +45,17 @@ export class EmoticonPickerComponent implements OnInit {
         var value = this.input.value;
         var selectionStart = this.input.selectionStart;
 
-        this.input.value = value.substr(0, selectionStart) + " :" + name + ": " + value.substr(selectionStart, value.length - selectionStart);
+        var insert = ":" + name + ": "
+
+        if (this.input.maxLength && value.length + insert.length > this.input.maxLength)
+            return this.utils.errorToast("Emoticon past niet meer", 2000);
+
+        this.input.value = value.substr(0, selectionStart) + insert + value.substr(selectionStart, value.length - selectionStart);
 
         this.input.focus();
-        this.input.selectionStart = this.input.selectionEnd = selectionStart + name.length + 4;
+        this.input.selectionStart = this.input.selectionEnd = selectionStart + insert.length;
+
+        this.inputChange.emit(this.input.value);
     }
 
     @HostListener('window:mouseup', ['$event'])
@@ -52,6 +79,29 @@ export class EmoticonPickerComponent implements OnInit {
         this.prevWindowWidth = window.innerWidth;
         this.prevLeft = Math.min(0, window.innerWidth - (r.left + r.width) - 8) + "px";
         return this.prevLeft;
+    }
+
+    private weirdCorrection = 74;
+
+    showCategory(id: number) {
+        var target = this.getCatDiv(id);
+        target.parentElement.scrollTop = target.offsetTop - this.weirdCorrection;
+    }
+
+    onScroll(scrollElement: Element) {
+
+        for (var cat of this.emoticons) {
+
+            var div = this.getCatDiv(cat.id);
+
+            if (scrollElement.scrollTop - div.offsetTop + this.weirdCorrection < -3) break;
+            
+            this.activeCatId = cat.id;
+        }
+    }
+
+    private getCatDiv(id: number) {
+        return document.getElementById('emoticon-picker-cat-' + id);
     }
 
 }
