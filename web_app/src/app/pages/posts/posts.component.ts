7067c7a82b0ef52bdad00b5225ac841234064a42
hilkojj <hilkojj@outlook.com>
2018-08-17 21:44:42 +0200

    carousel automatic sliding

--- web_app/src/app/pages/posts/posts.component.ts
+++ web_app/src/app/pages/posts/posts.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
+import { MaterializeAction } from '../../../../node_modules/angular2-materialize';
 
 @Component({
-  selector: 'app-posts',
-  templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit {
+export class PostsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    carouselActions = new EventEmitter<string | MaterializeAction>();
+    nextCarouselItemInterval;
+    
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+        this.startCarouselInterval();
+    }
+    
+    startCarouselInterval() {
+
+        this.nextCarouselItemInterval = setInterval(() => {
+    
+            this.carouselActions.emit({
+                action: "carousel",
+                params: ["next"]
+            });
+
+        }, 3000);
+    }
+
+    stopCarouselInterval() {
+
+        clearInterval(this.nextCarouselItemInterval);
+    }
+
+    ngOnDestroy() {
+
+        this.stopCarouselInterval();
+        
+    }
 
 }
