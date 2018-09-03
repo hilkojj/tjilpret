0bdef6144c29a951f539eb40f8e6e1d980c4f1d2
hilkojj <hilkojj@outlook.com>
2018-09-03 19:40:03 +0200

    i made my own fucking carousel because materializecss sucks a lot sometimes

--- web_app/src/app/pages/posts/posts.component.ts
+++ web_app/src/app/pages/posts/posts.component.ts
@@ -1,44 +1,131 @@
 import { Component, OnInit, OnDestroy, EventEmitter } from '@angular/core';
-import { MaterializeAction } from '../../../../node_modules/angular2-materialize';
+import { MaterializeAction } from 'angular2-materialize';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
+import { PostsService } from '../../services/posts.service';
+import { Post } from '../../models/post';
+
+interface PostsTab {
+    path: string;
+    name: string;
+    numberPrefix: string;
+    icon: string;
+    postsResolver: () => Observable<Post[]>,
+    posts?: Post[];
+}
 
 @Component({
     selector: 'app-posts',
     templateUrl: './posts.component.html',
     styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit, OnDestroy {
+export class PostsComponent implements OnInit {
 
-    carouselActions = new EventEmitter<string | MaterializeAction>();
-    nextCarouselItemInterval;
-    
-    constructor() { }
+    tabs: PostsTab[] = [
+        {
+            path: "top",
+            name: "Toppie uploods",
+            numberPrefix: "Toppie",
+            icon: "trending_up",
+            postsResolver: () => this.service.searchPosts("", "any", "score", true)
+        },
+
+        {
+            path: "niew",
+            name: "Niwe uploods",
+            numberPrefix: "Nieuw",
+            icon: "new_releases",
+            postsResolver: () => this.service.searchPosts("", "any", "time", true)
+        },
+
+        {
+            path: "niewsberichten",
+            name: "Nieuwsberichten",
+            numberPrefix: "Breaking news",
+            icon: "import_contacts",
+            postsResolver: () => this.service.searchPosts("", "any", "time", true, 3)
+        },
+
+        {
+            path: "meest-bekeken",
+            name: "Meest bekeken",
+            numberPrefix: "Meest bekeken",
+            icon: "remove_red_eye",
+            postsResolver: () => this.service.searchPosts("", "any", "views", true)
+        },
+
+        {
+            path: "pretbedervers",
+            name: "Pretbedervers",
+            numberPrefix: "Pretbedervend",
+            icon: "trending_down",
+            postsResolver: () => this.service.searchPosts("", "any", "score", false)
+        }
+    ];
+    postsLoaded = false;
+    currentTabI = 0;
+    featuredPostI = 0;
+    showCarouselPost = false;
+
+    constructor(
+        public service: PostsService,
+
+        private route: ActivatedRoute
+    ) { }
 
     ngOnInit() {
 
-        this.startCarouselInterval();
-    }
-    
-    startCarouselInterval() {
+        forkJoin(
+            this.tabs.map(tab => tab.postsResolver())
+        ).subscribe(allPosts => {
+            for (var i in allPosts)
+                this.tabs[i].posts = allPosts[i];
 
-        this.nextCarouselItemInterval = setInterval(() => {
-    
-            this.carouselActions.emit({
-                action: "carousel",
-                params: ["next"]
-            });
-
-        }, 3000);
+            this.postsLoaded = true;
+            setTimeout(() => {
+                this.showCarouselPost = true;
+            }, 20);
+        });
     }
 
-    stopCarouselInterval() {
-
-        clearInterval(this.nextCarouselItemInterval);
+    get tab(): PostsTab {
+        return this.tabs[this.currentTabI]
     }
 
-    ngOnDestroy() {
+    get featuredPost(): Post {
 
-        this.stopCarouselInterval();
+        return this.tab.posts[this.featuredPostI];
+    }
+
+    get shortFeaturedPostDescription(): string {
+
+        var p = this.featuredPost;
+
+        if (p.description.length > 100) return p.description.slice(0, 90) + "...";
+        return p.description;
+    }
+
+    prevFeaturedPost() {
+
+        this.showCarouselPost = false;
+
+        setTimeout(() => {
+            this.featuredPostI--;
+            if (this.featuredPostI < 0) this.featuredPostI = Math.min(7, this.tab.posts.length - 1);
+            this.showCarouselPost = true;
+        }, 400);
+
+    }
+
+    nextFeaturedPost() {
         
+        this.showCarouselPost = false;
+
+        setTimeout(() => {
+            this.featuredPostI++;
+            if (this.featuredPostI > Math.min(7, this.tab.posts.length - 1)) this.featuredPostI = 0;
+            this.showCarouselPost = true;
+        }, 400);
     }
 
 }
