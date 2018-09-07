c4bf4cc2fa96805c21265507e738c80ed1fdd8a1
hilkojj <hilkojj@outlook.com>
2018-09-07 13:13:17 +0200

    weird search box with way too much css

--- web_app/src/app/pages/chat/chat.component.ts
+++ web_app/src/app/pages/chat/chat.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+    selector: 'app-chat',
+    templateUrl: './chat.component.html',
+    styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    convSearchQuery = "";
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+    }
+    
 }
