93c35c6a3850206b5cfd53230321f710f7d99919
hilkojj <hilkojj@outlook.com>
2018-09-08 19:10:32 +0200

    group pic

--- web_app/src/app/components/group-pic/group-pic.component.ts
+++ web_app/src/app/components/group-pic/group-pic.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Conversation } from '../../models/chat';
+import { PRIVATE_CONTENT_URL } from '../../constants';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-group-pic',
-  templateUrl: './group-pic.component.html',
-  styleUrls: ['./group-pic.component.scss']
+    selector: 'app-group-pic',
+    templateUrl: './group-pic.component.html',
+    styleUrls: ['./group-pic.component.scss']
 })
 export class GroupPicComponent implements OnInit {
 
-  constructor() { }
+    @Input() conv: Conversation;
+    @Input() size: string;
+    @Input() dim: string = "med";
+    @Input() borderWidth: string = "0";
+    @Input() cursor: string = "default";
 
-  ngOnInit() {
-  }
+    constructor(
+        private san: DomSanitizer
+    ) { }
+
+    ngOnInit() {
+    }
+
+    get url(): string {
+        if (this.conv.groupPic == null) return "/assets/img/default_group_pic.png";
+        return PRIVATE_CONTENT_URL + "group_pic/" + this.dim + "/" + this.conv.groupPic;
+    }
 
 }
--- web_app/src/app/constants.ts
+++ web_app/src/app/constants.ts
@@ -1,4 +1,5 @@
 export const SITE_URL: string = "https://tjilpret.tk/";
 export const API_URL: string = SITE_URL + "api/";
 export const CONTENT_URL: string = SITE_URL + "static_content/";
+export const PRIVATE_CONTENT_URL: string = SITE_URL + "private_content/";
 export const THEME_COLOR = { r: 94, g: 94, b: 255 };
