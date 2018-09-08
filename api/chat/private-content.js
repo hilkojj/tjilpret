3cf3cebdbdb54c94227223210e41615d3bc2e21f
hilkojj <hilkojj@outlook.com>
2018-09-08 16:48:42 +0200

    chat wallpaper

--- api/chat/private-content.js
+++ api/chat/private-content.js
@@ -3,6 +3,40 @@ var path = require('path');
 
 module.exports = privateContent => {
 
+    privateContent.get("/chat_wallpaper/:filename", (req, res) => {
+
+        var token = req.cookies.token | 0;
+        var filename = String(req.params.filename);
+
+        if (!token) {
+            res.setHeader("Cache-Control", "no-cache");
+            return res.status(400).send("No token cookie found");
+        }
+
+        db.connection.query(`
+            SELECT wallpaper FROM users
+            JOIN tokens ON token = ? AND tokens.user_id = users.user_id
+            WHERE wallpaper = ?`, [token, filename], (err, rows, fields) => {
+
+                if (err) {
+                    console.log(err);
+                    res.setHeader("Cache-Control", "no-cache");
+                    return res.status(500).send("Oepsie er ging iets mis!?!?");
+                }
+
+                if (rows.length == 0) {
+                    res.setHeader("Cache-Control", "no-cache");
+                    return res.status(403).send("Jij mag deze foto nit zien!!");
+                }
+                filename = rows[0].wallpaper;
+                res.setHeader("Cache-Control", "private, only-if-cached, max-age=999999");
+                res.sendFile(path.resolve(`${__dirname}/../private_content/chat_wallpapers/${filename}`));
+
+            }
+        );
+
+    });
+
     privateContent.get("/group_pic/:dim/:filename", (req, res) => {
 
         var token = req.cookies.token | 0;
