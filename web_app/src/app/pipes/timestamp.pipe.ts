bdde896408220b22913e1efa0fa3a836efc919d2
hilkojj <hilkojj@outlook.com>
2018-09-09 22:09:30 +0200

    'short timestamp pipe'

--- web_app/src/app/pipes/timestamp.pipe.ts
+++ web_app/src/app/pipes/timestamp.pipe.ts
@@ -5,27 +5,44 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimestampPipe implements PipeTransform {
 
-    transform(timestamp: number, args?: any): string {
+    transform(timestamp: number, option?: string): string {
 
         var a = new Date(timestamp * 1000);
         var b = new Date();
-        var months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+
         var year = a.getFullYear();
-        var month = months[a.getMonth()];
+        var month = a.getMonth();
         var date = a.getDate();
         var hour = a.getHours();
         var min: any = a.getMinutes();
         var string = '';
+
+        if (option == "short") {
+
+            var thisYear = year == b.getFullYear();
+            var thisMonth = a.getMonth() == b.getMonth();
+
+            if (thisYear && thisMonth) {
+
+                if (date == b.getDate() -1) return "Gisteren";
+                if (date == b.getDate()) return hour + ":" + min;
+            }
+
+            return `${date}-${month + 1}-${year}`;
+        }
+
+        var months = ['januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december'];
+        
         if (year == b.getFullYear()) {
-            if (date != b.getDate() || month != months[b.getMonth()]) {
-                if (month == months[b.getMonth()] && b.getDate() - date == 1) {
+            if (date != b.getDate() || month != b.getMonth()) {
+                if (month == b.getMonth() && b.getDate() - date == 1) {
                     string += 'Gisteren ';
                 } else {
-                    string += date + ' ' + month + ' ';
+                    string += date + ' ' + months[month] + ' ';
                 }
             }
         } else {
-            string += date + ' ' + month + ' ' + year + ' ';
+            string += date + ' ' + months[month] + ' ' + year + ' ';
         }
         if (min < 10) {
             min = '0' + min;
